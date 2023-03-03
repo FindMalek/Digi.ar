f267d633714618f4117738ea8186c9fd6fd3dbe2
FindMalek <malekgarahellalbus@gmail.com>
2023-03-04 00:32:33 +0100

    (1.2.1) - Fixed Portfolio navigation and fixed navbar

--- js/navbar-scroll.js
+++ js/navbar-scroll.js
@@ -1,11 +1,21 @@
 const navbar = document.querySelector('.site-navbar');
 
+if (window.location.href.endsWith('index.html')) {
+  navbar.classList.add('white-text');
+}
+
 window.addEventListener('scroll', () => {
-  if (window.scrollY > 50) {
+  const scrollPosition = window.scrollY;
+
+  if (scrollPosition > 0) {
     navbar.classList.add('white-bg');
     navbar.querySelector('.site-navigation.border-bottom').style.borderBottom = 'none';
+    navbar.classList.remove('white-text');
   } else {
     navbar.classList.remove('white-bg');
-    navbar.querySelector('.site-navigation.border-bottom').style.borderBottom = '1px solid #f3f3f4';
+    navbar.querySelector('.site-navigation.border-bottom').style.borderBottom = '0px solid #f3f3f4';
+    if (window.location.href.endsWith('index.html')) {
+      navbar.classList.add('white-text');
+    }
   }
 });
