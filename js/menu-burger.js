55f82e836d55d31061ceac615504b84e18ba500f
FindMalek <malekgarahellalbus@gmail.com>
2023-03-21 13:52:17 +0100

    (2.0.1) - Header in beta version

---
+++ js/menu-burger.js
@@ -0,0 +1,6 @@
+const menuBurger = document.querySelector(".menu-burger");
+const navLinks = document.querySelector(".nav-links");
+
+menuBurger.addEventListener("click", () => {
+  navLinks.classList.toggle("mobile-menu");
+});
