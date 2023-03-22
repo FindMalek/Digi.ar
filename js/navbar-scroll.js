83ab8368bad0ec539566e9977d73571fe05c9eb4
FindMalek <malekgarahellalbus@gmail.com>
2023-03-22 01:18:20 +0100

    (2.1.0) - Revamp Header Responsive.

--- js/navbar-scroll.js
+++ js/navbar-scroll.js
@@ -1,26 +1,9 @@
-$(function() {
-  $("#header").load("includes/header.html", function() {
-    const navbar = document.querySelector('.site-navbar');
+var navbar = document.querySelector('.navbar');
 
-    if (window.location.href.endsWith('index.html')) {
-      navbar.classList.add('white-text');
-    }
-
-    window.addEventListener('scroll', () => {
-      const scrollPosition = window.scrollY;
-
-      if (scrollPosition > 0) {
-        navbar.classList.add('white-bg');
-        navbar.querySelector('.site-navigation.border-bottom').style.borderBottom = 'none';
-        navbar.classList.remove('white-text');
-      }
-      else {
-        navbar.classList.remove('white-bg');
-        navbar.querySelector('.site-navigation.border-bottom').style.borderBottom = '0px solid #f3f3f4';
-        if (window.location.href.endsWith('index.html')) {
-          navbar.classList.add('white-text');
-        }
-      }
-    });
-  });
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 0) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
 });
