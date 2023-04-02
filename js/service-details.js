2ae7ec351fb3675278e5dd42e334f5863e85caec
FindMalek <malekgarahellalbus@gmail.com>
2023-04-02 16:35:57 +0100

    (2.5.1) - Added Nova 5i Resolution.

--- js/service-details.js
+++ js/service-details.js
@@ -1,32 +1,26 @@
-// Get all the Learn More anchor tags
+
 const learnMoreLinks = document.querySelectorAll('.learn-more');
 
-// Add a click event listener to each link
 learnMoreLinks.forEach(link => {
     link.addEventListener('click', (event) => {
-        event.preventDefault(); // Prevent default anchor tag behavior
+        event.preventDefault();
 
-        // Extract the name of the service from the link ID
         const serviceName = link.id.split('-')[0] + '-' + link.id.split('-')[1];
 
-        // Construct the URL of the service details page
         const url = `src/services-details/${serviceName}-section.html`;
 
-        // Call the loadServiceDetails() function with the URL
         loadServiceDetails(url);
     });
 });
 
 function loadServiceDetails(url) {
-    // Use fetch API to get the service details HTML
     fetch(`./${url}`)
         .then(response => response.text())
         .then(html => {
-            // Insert the service details HTML into the #service-details element
+
             const serviceDetails = document.getElementById('service-details');
             serviceDetails.innerHTML = html;
 
-            // Use the scrollTo() method to smoothly scroll down to the service details section
             const offsetTop = serviceDetails.offsetTop;
             window.scrollTo({
                 top: offsetTop,
