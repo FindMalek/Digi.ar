ebad692c4f9a965e00b49a9e0604f63e6ffb97f9
FindMalek <malekgarahellalbus@gmail.com>
2023-04-06 22:13:18 +0100

    (3.2.1) - Fixed Portfolio: Filter working

--- js/menu-burger.js
+++
@@ -1,6 +0,0 @@
-const menuBurger = document.querySelector(".menu-burger");
-const navLinks = document.querySelector(".nav-links");
-
-menuBurger.addEventListener("click", () => {
-    navLinks.classList.toggle("mobile-menu");
-});
--- js/portfolio.js
+++ js/portfolio.js
@@ -1,12 +1,10 @@
 $(document).ready(function() {
-    // Activate the first filter on page load
     $('.project-gallery-menu li:first').addClass('active');
-    $('.gallery-item').fadeIn(); // show all gallery items initially
+    $('.gallery-item').fadeIn();
 
     $('.filter').click(function() {
         var category = $(this).data('filter');
 
-        // If the clicked category is already active, exit the function
         if ($(this).hasClass('active')) {
             return;
         }
@@ -20,42 +18,36 @@ $(document).ready(function() {
             }, 300);
         }
 
-        // Activate the clicked filter and deactivate others
         $('.filter').removeClass('active');
         $(this).addClass('active');
     });
 });
 
-// Wait for the DOM to be fully loaded
 document.addEventListener("DOMContentLoaded", function() {
-    
-    // Get all the filter buttons
     const filterButtons = document.querySelectorAll(".filter");
-    
-    // Get all the gallery items
     const galleryItems = document.querySelectorAll(".gallery-item");
-    
-    // Attach a click event listener to each filter button
+
     filterButtons.forEach((button) => {
         button.addEventListener("click", function() {
-            // Get the filter category
             const filterCategory = this.getAttribute("data-filter");
-            
-            // Loop through all gallery items
-            galleryItems.forEach((item) => {
-                console.log('Display:', item.style.display);
-                // Get the list of classes of the current gallery item
-                const classes = item.getAttribute("class");
-                
-                // Check if the filter category is present in the list of classes
-                if (classes.split(' ').map(c => c.trim()).includes(filterCategory.substring(1)) || filterCategory === "all") {
-                  item.style.display = "inline-block";
-                } else {
-                  item.style.display = "none";
-                }
-                console.log('Display:', item.style.display);
-              });
 
+            if ($(this).hasClass('active')) {
+                return;
+            }
+
+            galleryItems.forEach((item) => {
+                item.style.display = "none";
+            });
+
+            setTimeout(() => {
+                galleryItems.forEach((item) => {
+                    const classes = item.getAttribute("class");
+
+                    if (classes.split(' ').map(c => c.trim()).includes(filterCategory.substring(1)) || filterCategory === "all") {
+                        item.style.display = "inline-block";
+                    }
+                });
+            }, 0);
         });
     });
 });
