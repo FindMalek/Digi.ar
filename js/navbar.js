d91143ae6ccf3ae993e34691e61bd9e9bfaf9384
FindMalek <malekgarahellalbus@gmail.com>
2023-04-06 04:10:34 +0100

    (3.0.0) - Updating the theme, Updated footer, Updated Home page and Updated Navbar

--- js/navbar.js
+++ js/navbar.js
@@ -1,27 +1,80 @@
 $(document).ready(function() {
-  var navbar = $("#navbar");
-  var navbarLogo = $(".navbarLogo");
-  var navLinks = $(".nav-link");
+    var navbar = $("#navbar");
+    var navbarLogo = $(".navbarLogo");
+    var navLinks = $(".nav-link");
 
-  if ($(window).width() > 992 && navbarLogo.attr("src") === "images/Digital Army - white.svg") {
-      navbarLogo.attr("src", "images/Digital Army - black.svg");
-  }
+    if ($(window).width() > 992 && navbarLogo.attr("src") === "images/Digital Army - white.svg") {
+        navbarLogo.attr("src", "images/Digital Army - black.svg");
+    }
 
-  $(window).scroll(function() {
-      if ($(window).width() > 992) {
-          if ($(window).scrollTop() > 50) {
-              navbar.addClass("scroll");
-              navbarLogo.attr("src", "images/Digital Army - white.svg");
-              navLinks.removeClass("text-dark").addClass("text-light");
-          } else {
-              navbar.removeClass("scroll");
-              navbarLogo.attr("src", "images/Digital Army - black.svg");
-              navLinks.removeClass("text-light").addClass("text-dark");
-          }
-      } else if ($(window).width() > 992) {
-          navbar.removeClass("scroll");
-          navbarLogo.attr("src", "images/Digital Army - black.svg");
-          navLinks.removeClass("text-light").addClass("text-dark");
-      }
-  });
+    $(window).scroll(function() {
+        if ($(window).width() > 992) {
+            if ($(window).scrollTop() > 50) {
+                navbar.addClass("scroll");
+                navbarLogo.attr("src", "images/Digital Army - white.svg");
+                navLinks.removeClass("text-dark").addClass("text-light");
+            } else {
+                navbar.removeClass("scroll");
+                navbarLogo.attr("src", "images/Digital Army - black.svg");
+                navLinks.removeClass("text-light").addClass("text-dark");
+            }
+        } else if ($(window).width() > 992) {
+            navbar.removeClass("scroll");
+            navbarLogo.attr("src", "images/Digital Army - black.svg");
+            navLinks.removeClass("text-light").addClass("text-dark");
+        }
+    });
+
+    // Show/hide the submenu when hovering over the "About" link
+    $("#about").hover(function() {
+        $(".dropdown-menu").show();
+    }, function() {
+        $(".dropdown-menu").hide();
+    });
+
+    // Add/remove the active class when hovering over the submenu items
+    $(".dropdown-menu li a").hover(function() {
+        $(this).addClass("active");
+    }, function() {
+        $(this).removeClass("active");
+    });
+
+    // Smooth scrolling for menu links
+    $('a.nav-link').on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+                window.location.hash = hash;
+            });
+        }
+        // Hide mobile menu after clicking on a link
+        $('.navbar-collapse').collapse('hide');
+    });
+});
+
+// Get the About dropdown menu item
+var aboutDropdown = document.querySelector('#navbarDropdownMenuLink');
+
+// Get the navbar collapse element
+var navbarCollapse = document.querySelector('#navbarToggleExternalContent');
+
+// Add a click event listener to the About dropdown menu item
+aboutDropdown.addEventListener('click', function(event) {
+    // Toggle the visibility of the submenu
+    var submenu = this.nextElementSibling;
+    submenu.classList.toggle('show');
+
+    // Remove the 'show' class from the navbar collapse element
+    var otherSubmenus = document.querySelectorAll('.dropdown-menu.show');
+    otherSubmenus.forEach(function(item) {
+        if (item != submenu) {
+            item.classList.remove('show');
+        }
+    });
+    
+    // Prevent the default behavior of the link
+    event.preventDefault();
 });
