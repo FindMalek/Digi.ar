0f792119e2b5aeb641216ece17a34cb2ee2a43c7
FindMalek <malekgarahellalbus@gmail.com>
2023-04-18 16:10:21 +0100

    (3.4.7) - Fixed Font Weight and Navbar.

--- js/navbar.js
+++ js/navbar.js
@@ -27,46 +27,66 @@ $(document).ready(function() {
 
     // Show/hide the submenu when hovering over the "About" link
     $("#about").hover(function() {
-        $(".dropdown-menu").show();
-    }, function() {
-        $(".dropdown-menu").hide();
-    });
+            $(".dropdown-menu").show();
+        }
+
+        ,
+        function() {
+            $(".dropdown-menu").hide();
+        });
 
     // Add/remove the active class when hovering over the submenu items
     $(".dropdown-menu li a").hover(function() {
-        $(this).addClass("active");
-    }, function() {
-        $(this).removeClass("active");
+            $(this).addClass("active");
+        }
+
+        ,
+        function() {
+            $(this).removeClass("active");
+        });
+
+    // Get the About dropdown menu item
+    var aboutDropdown = document.querySelector('#navbarDropdownMenuLink');
+
+    // Get the navbar collapse element
+    var navbarCollapse = document.querySelector('#navbarToggleExternalContent');
+
+    // Add a click event listener to the About dropdown menu item
+    aboutDropdown.addEventListener('click', function(event) {
+        // Toggle the visibility of the submenu
+        var submenu = this.nextElementSibling;
+        submenu.classList.toggle('show');
+
+        // Remove the 'show' class from the navbar collapse element
+        var otherSubmenus = document.querySelectorAll('.dropdown-menu.show');
+
+        otherSubmenus.forEach(function(item) {
+            if (item != submenu) {
+                item.classList.remove('show');
+            }
+        });
+
+        // Prevent the default behavior of the link
+        event.preventDefault();
     });
 
     // Trigger the collapse toggle button to close the navbar
     $(".nav-link").click(function() {
-        setTimeout(function() {
-            $("#navbarToggleExternalContent").collapse("hide");
-        }, 200);
-    });
-});
+        if ($(this).attr('id') !== 'navbarDropdownMenuLink') {
+            setTimeout(function() {
+                    $("#navbarToggleExternalContent").collapse("hide");
+                }
 
-// Get the About dropdown menu item
-var aboutDropdown = document.querySelector('#navbarDropdownMenuLink');
-
-// Get the navbar collapse element
-var navbarCollapse = document.querySelector('#navbarToggleExternalContent');
-
-// Add a click event listener to the About dropdown menu item
-aboutDropdown.addEventListener('click', function(event) {
-    // Toggle the visibility of the submenu
-    var submenu = this.nextElementSibling;
-    submenu.classList.toggle('show');
-
-    // Remove the 'show' class from the navbar collapse element
-    var otherSubmenus = document.querySelectorAll('.dropdown-menu.show');
-    otherSubmenus.forEach(function(item) {
-        if (item != submenu) {
-            item.classList.remove('show');
+                , 200);
         }
     });
 
-    // Prevent the default behavior of the link
-    event.preventDefault();
+    // Add a click event listener to the "About Us" dropdown menu item
+    $('#navbarDropdownMenuLink + .dropdown-menu a:first-child').click(function() {
+        setTimeout(function() {
+                $("#navbarToggleExternalContent").collapse("hide");
+            }
+
+            , 200);
+    });
 });
