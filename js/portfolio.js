14ad3fab377935db03e92c488c1660110a36c3a1
FindMalek <malekgarahellalbus@gmail.com>
2023-05-15 11:21:19 +0100

    (5.1.1) - Portfolio Update - Gallery Items

--- js/portfolio.js
+++ js/portfolio.js
@@ -6,18 +6,67 @@ $(document).ready(function() {
 
         if (filter == 'all') {
             $('.gallery-item').show(400);
-        }
-        else {
+        } else {
             $('.gallery-item').not('.' + filter).hide(200);
             $('.gallery-item').filter('.' + filter).show(400);
         }
     });
 
     $('.gallery-item').magnificPopup({
-        delegate: 'a',
-        type: 'image',
+        delegate: '.pp:visible',
+        type: 'ajax',
+        mainClass: "mfp-fade",
+        closeBtnInside: true,
+        midClick: true,
         gallery: {
-            enabled: true
+            enabled: true,
+            navigateByImgClick: true,
+        },
+        callbacks: {
+            ajaxContentAdded: function() {
+                $(".owl-carousel").each(function(index) {
+                    var a = $(this);
+
+                    $(this).owlCarousel({
+                        autoplay: a.data('autoplay'),
+                        center: a.data('center'),
+
+                        autoplayTimeout: a.data('autoplaytimeout'),
+                        autoplayHoverPause: a.data('autoplayhoverpause'),
+                        lazyLoad: a.data('lazyload'),
+
+                        loop: a.data('loop'),
+                        speed: a.data('speed'),
+                        slideBy: a.data('slideby'),
+
+                        dots: a.data('dots'),
+                        autoHeight: a.data('autoheight'),
+                        autoWidth: a.data('autowidth'),
+                        margin: a.data('margin'),
+                        stagePadding: a.data('stagepadding'),
+                        
+                        navText: ['<i class="fa fa-chevron-left"></i>', '<i class="fa fa-chevron-right"></i>'],
+                        animateOut: a.data('animateOut'),
+                        animateIn: a.data('animateIn'),
+                        video: a.data('video'),
+                        items: a.data('items'),
+                        responsive: {
+                            0: {
+                                items: a.data('items-xs'),
+                            },
+                            576: {
+                                items: a.data('items-sm'),
+                            },
+                            768: {
+                                items: a.data('items-md'),
+                            },
+                            992: {
+                                items: a.data('items-lg'),
+                            }
+                        }
+                    });
+                });
+            }
         }
     });
 });
