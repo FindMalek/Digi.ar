2204ca2ed608646566f57ebafd9e9e874cb2a98f
FindMalek <malekgarahellalbus@gmail.com>
2023-05-15 10:56:14 +0100

    (5.0.1) - Portfolio Update - Smoother experience for filtering.

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,53 +1,23 @@
 $(document).ready(function() {
-    $('.project-gallery-menu li:first').addClass('active');
-    $('.gallery-item').fadeIn();
-
     $('.filter').click(function() {
-        var category = $(this).data('filter');
+        $(this).addClass('active').siblings().removeClass('active');
 
-        if ($(this).hasClass('active')) {
-            return;
+        var filter = $(this).attr('data-filter');
+
+        if (filter == 'all') {
+            $('.gallery-item').show(400);
         }
-
-        if (category == 'all') {
-            $('.gallery-item').fadeIn();
-        } else {
-            $('.gallery-item').fadeOut();
-            setTimeout(function() {
-                $('.gallery-item-' + category).fadeIn();
-            }, 300);
+        else {
+            $('.gallery-item').not('.' + filter).hide(200);
+            $('.gallery-item').filter('.' + filter).show(400);
         }
-
-        $('.filter').removeClass('active');
-        $(this).addClass('active');
     });
-});
 
-document.addEventListener("DOMContentLoaded", function() {
-    const filterButtons = document.querySelectorAll(".filter");
-    const galleryItems = document.querySelectorAll(".gallery-item");
-
-    filterButtons.forEach((button) => {
-        button.addEventListener("click", function() {
-            const filterCategory = this.getAttribute("data-filter");
-
-            if ($(this).hasClass('active')) {
-                return;
-            }
-
-            galleryItems.forEach((item) => {
-                item.style.display = "none";
-            });
-
-            setTimeout(() => {
-                galleryItems.forEach((item) => {
-                    const classes = item.getAttribute("class");
-                  
-                    if (classes.split(' ').map(c => c.trim()).includes(filterCategory.substring(1)) || filterCategory === "all") {
-                        item.style.display = "inline-block";
-                    }
-                  });
-            }, 100);
-        });
+    $('.gallery-item').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        gallery: {
+            enabled: true
+        }
     });
 });
