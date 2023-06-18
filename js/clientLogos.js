8368fb7fc3626b47e7feebe31bcbb84026592e16
FindMalek <malekgarahellalbus@gmail.com>
2023-06-19 00:14:26 +0100

    (6.0.5.4) - Fixing Github Image Loader.

--- js/clientLogos.js
+++ js/clientLogos.js
@@ -1,27 +1,25 @@
+const folderPath = "images/clients/";
+const logoLinks = [];
+
+for (let i = 1; i <= 27; i++) {
+  const fileName = `${i}.png`;
+  logoLinks.push(fileName);
+}
+
 const clientsSlides = document.querySelector(".clients__slides");
 
-fetch("images/clients/")
-  .then((response) => response.text())
-  .then((html) => {
-    const tempDiv = document.createElement("div");
-    tempDiv.innerHTML = html;
-
-    const logoLinks = tempDiv.querySelectorAll("a");
-
-    logoLinks.forEach((link) => {
-      fetch(link.href)
-        .then((response) => response.blob())
-        .then((blob) => {
-          const objectURL = URL.createObjectURL(blob);
-          const logoImg = document.createElement("img");
-          logoImg.src = objectURL;
-          logoImg.style.filter = "grayscale(100%)";
-          logoImg.style.padding = "20px";
-          logoImg.style.maxHeight = "150px";
-          logoImg.style.width = "auto";
-          clientsSlides.appendChild(logoImg);
-        })
-        .catch((error) => console.error(error));
-    });
-  })
-  .catch((error) => console.error(error));
+logoLinks.forEach((fileName) => {
+  fetch(`${folderPath}${fileName}`)
+    .then((response) => response.blob())
+    .then((blob) => {
+      const objectURL = URL.createObjectURL(blob);
+      const logoImg = document.createElement("img");
+      logoImg.src = objectURL;
+      logoImg.style.filter = "grayscale(100%)";
+      logoImg.style.padding = "20px";
+      logoImg.style.maxHeight = "150px";
+      logoImg.style.width = "auto";
+      clientsSlides.appendChild(logoImg);
+    })
+    .catch((error) => console.error(error));
+});
