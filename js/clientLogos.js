4bed6992eff59b2a37126284e28aa70bc3206293
FindMalek <malekgarahellalbus@gmail.com>
2023-06-18 23:47:06 +0100

    (6.0.5.3) - Fixing Github Image Loader.

--- js/clientLogos.js
+++ js/clientLogos.js
@@ -1,25 +1,27 @@
 const clientsSlides = document.querySelector(".clients__slides");
 
-// Fetch the logo images from the directory
-fetch("../images/clients/")
+fetch("images/clients/")
   .then((response) => response.text())
   .then((html) => {
-    // Create a temporary div element to parse the fetched HTML
     const tempDiv = document.createElement("div");
     tempDiv.innerHTML = html;
 
-    // Select all the <a> elements (logo images) within the fetched HTML
     const logoLinks = tempDiv.querySelectorAll("a");
 
-    // Loop through each logo link and create <img> elements
     logoLinks.forEach((link) => {
-        const logoImg = document.createElement("img");
-        logoImg.src = link.href;
-        logoImg.style.filter = "grayscale(100%)";
-        logoImg.style.padding = "20px";
-        logoImg.style.maxHeight = "150px";
-        logoImg.style.width = "auto";
-        clientsSlides.appendChild(logoImg);
-      });
+      fetch(link.href)
+        .then((response) => response.blob())
+        .then((blob) => {
+          const objectURL = URL.createObjectURL(blob);
+          const logoImg = document.createElement("img");
+          logoImg.src = objectURL;
+          logoImg.style.filter = "grayscale(100%)";
+          logoImg.style.padding = "20px";
+          logoImg.style.maxHeight = "150px";
+          logoImg.style.width = "auto";
+          clientsSlides.appendChild(logoImg);
+        })
+        .catch((error) => console.error(error));
+    });
   })
   .catch((error) => console.error(error));
