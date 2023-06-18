b47b84ea526e064525762b32a8634213870ef195
FindMalek <malekgarahellalbus@gmail.com>
2023-06-19 00:21:42 +0100

    (6.0.5.6) - Fixing Github Image Loader.

--- js/clientLogos.js
+++ js/clientLogos.js
@@ -1,25 +1,31 @@
-const folderPath = "images/clients/";
+const folderPath = "https://findmalek.github.io/Digi.ar/images/clients/";
 const logoLinks = [];
 
-for (let i = 1; i <= 27; i++) {
-  const fileName = `${i}.png`;
-  logoLinks.push(fileName);
-}
+fetch(folderPath)
+  .then((response) => response.text())
+  .then((html) => {
+    const tempDiv = document.createElement("div");
+    tempDiv.innerHTML = html;
 
-const clientsSlides = document.querySelector(".clients__slides");
+    const fileLinks = tempDiv.querySelectorAll("a");
+    fileLinks.forEach((link) => {
+      const fileName = decodeURIComponent(link.getAttribute("href").substring(1));
+      if (fileName.endsWith(".png")) {
+        logoLinks.push(fileName);
+      }
+    });
 
-logoLinks.forEach((fileName) => {
-  fetch(`./${folderPath}${fileName}`)
-    .then((response) => response.blob())
-    .then((blob) => {
-      const objectURL = URL.createObjectURL(blob);
+    const clientsSlides = document.querySelector(".clients__slides");
+
+    logoLinks.forEach((fileName) => {
+      const imageUrl = `${folderPath}${fileName}`;
       const logoImg = document.createElement("img");
-      logoImg.src = objectURL;
+      logoImg.src = imageUrl;
       logoImg.style.filter = "grayscale(100%)";
       logoImg.style.padding = "20px";
       logoImg.style.maxHeight = "150px";
       logoImg.style.width = "auto";
       clientsSlides.appendChild(logoImg);
-    })
-    .catch((error) => console.error(error));
-});
+    });
+  })
+  .catch((error) => console.error(error));
