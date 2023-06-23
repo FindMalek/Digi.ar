70afae545baa1706c7f1fa0b2fc1e836458dd611
FindMalek <malekgarahellalbus@gmail.com>
2023-06-23 22:46:20 +0100

    Fix

--- js/clientLogos.js
+++ js/clientLogos.js
@@ -7,25 +7,28 @@ for (let i = 1; i <= 26; i++) {
 }
 
 const logoLinksTwice = [...logoLinks, ...logoLinks];
-
+console.log("Logo twice");
 console.log(logoLinksTwice);
 
 const clientsSlides = document.querySelector(".clients__slides");
 
 // Fetch and append images in order
-for (let i = 0; i < logoLinksTwice.length; i++) {
-  const fileName = logoLinksTwice[i];
-  fetch(`./${folderPath}${fileName}`)
-    .then((response) => response.blob())
-    .then((blob) => {
-      const objectURL = URL.createObjectURL(blob);
-      const logoImg = document.createElement("img");
-      logoImg.src = objectURL;
-      logoImg.style.filter = "grayscale(100%)";
-      logoImg.style.padding = "30px";
-      logoImg.style.maxHeight = "200px";
-      logoImg.style.width = "auto";
-      clientsSlides.appendChild(logoImg);
-    })
-    .catch((error) => console.error(error));
-}
+Promise.all(
+  logoLinksTwice.map((fileName) => {
+    console.log("Logo twice");
+    console.log(logoLinksTwice);
+    return fetch(`./${folderPath}${fileName}`)
+      .then((response) => response.blob())
+      .then((blob) => {
+        const objectURL = URL.createObjectURL(blob);
+        const logoImg = document.createElement("img");
+        logoImg.src = objectURL;
+        logoImg.style.filter = "grayscale(100%)";
+        logoImg.style.padding = "30px";
+        logoImg.style.maxHeight = "200px";
+        logoImg.style.width = "auto";
+        clientsSlides.appendChild(logoImg);
+      })
+      .catch((error) => console.error(error));
+  })
+);
