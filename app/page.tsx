8f8ac9fda81a0326855a370e37f829a8b7fd0979
FindMalek <malekgarahellalbus@gmail.com>
2023-08-15 12:49:44 +0100

    (1.0.2) - Herosection Implementation | Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 "use client"
 
 import Navbar from '@/components/overall/Navbar'
+import Herosection from '@/components/home/Herosection'
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <Herosection />
     </>
   )
 }
